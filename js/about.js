bcc5772f9e3c448395ace806daf5e0c482240398
Mujadded Al Rabbani Alif <Mujadded.alif@gmail.com>
2018-08-25 18:11:14 +0600

    changed about it

--- js/about.js
+++ js/about.js
@@ -41,4 +41,43 @@ $(document).ready(function() {
       }
     }
   });
+
+  var typeIt = function(element,txt,start){
+    var $el = $(element);
+    var humanize = Math.round(Math.random() * (100 - 30)) + 30;
+    var txtLen = txt.length;
+    var timeOut = setTimeout(function() {
+      var type = txt.substring(0, start);
+      start++;
+      $el.text(type + "_");
+      typeIt(element,txt,start);
+      if (type.length >= txtLen) {
+        $el.text($el.text().slice(0, -1)); // remove the '|'
+        clearTimeout(timeOut);
+      }
+    }, humanize);
+  };
+
+var showQuote = function() {
+  var yql_url = "https://query.yahooapis.com/v1/public/yql";
+  var url = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
+  $.ajax({
+    url: yql_url,
+    data: {
+      q: 'SELECT * FROM json WHERE url="' + url + '"',
+      format: "json",
+      jsonCompat: "new"
+    },
+    dataType: "json",
+    success: function(response) {    
+      var quote = response.query.results.json.quoteText;
+      var author = response.query.results.json.quoteAuthor;
+      quote = quote.slice(0,-1);
+      txt = '"'+ quote +'"'+"-"+author;
+      
+      typeIt(".writer",txt,0);
+    }
+  });
+};
+showQuote();
 }); // close out script
